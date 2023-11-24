efe25166904ddf160089df1d874eaa8dd6354e31
leiber bertel <leiberbertel777@gmail.com>
2023-11-24 12:11:16 -0500

    feat: add saving functionality

---
+++ app/guardar.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "../config.php";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombres = $_POST['nombres'];
+    $paterno = $_POST['paterno'];
+    $materno = $_POST['materno'];
+
+    $sql = "INSERT INTO alumnos (nombres, apellido_paterno, apellido_materno)
+            VALUES ('$nombres', '$paterno', '$materno')";
+
+    if ($con->query($sql) === TRUE) {
+        header("Location: listaEstudiantes.php");
+        exit();
+    } else {
+        echo "Error al guardar los datos: " . $con->error;
+    }
+
+    $conn->close();
+}
+?>
