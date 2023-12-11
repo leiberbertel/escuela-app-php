8f4e7cd139ef13e5b79fdf0258865928b8a41868
leiber bertel <leiberbertel777@gmail.com>
2023-12-11 18:39:54 -0500

    refactor: update models

--- schema_db/escuela.sql
+++ schema_db/escuela.sql
@@ -2,9 +2,23 @@ CREATE DATABASE escuela;
 
 USE escuela;
 
-CREATE TABLE alumnos(
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE alumnos (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nombres VARCHAR(200) NOT NULL,
-    apellido_materno VARCHAR(50) NOT NULL,
-    apellido_paterno VARCHAR(50),
+    apellido_paterno VARCHAR(50) NOT NULL,
+    apellido_materno VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE rol (
+    idrol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    rol VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE usuario (
+    idusuario int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(40) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    usuario VARCHAR(40) NOT NULL,
+    rol INT(11) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL
 );
