4a023442ee72d0626192056f8140bca63de9cce7
leiber bertel <leiberbertel777@gmail.com>
2023-12-11 17:20:49 -0500

    refactor: change the path to the files

---
+++ src/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-3 my-5">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="../app/register.php" class="nav-link px-2">Registro</a></li>
+        <li class="nav-item"><a href="../app/listaEstudiantes.php" class="nav-link px-2">Lista de estudiantes</a></li>
+      </ul>
+      <p class="text-center">&copy; 2023 <a class="text-light" target="_blank" href="https://leiberbertel.github.io">Leiber Bertel</a></p>
+</footer>
---
+++ src/includes/nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-sm  nav-header">
+    <div class="container-fluid">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="../app/register.php">Registro
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../app/listaEstudiantes.php">Lista de estudiantes
+          </a>
+        </li>
+      </ul>
+    </div>
+</nav>
