448d6230ad5b5f0f7271d83873701cb760bd23a8
leiber bertel <leiberbertel777@gmail.com>
2023-12-11 19:58:52 -0500

    feat: add active session validation

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,3 +1,12 @@
+<?php 
+
+session_start();
+
+if(empty($_SESSION['active'])) {
+    header ('location:../../index.php');
+}
+?>
+
 <nav class="navbar navbar-expand-sm  nav-header">
     <div class="container-fluid">
       <ul class="navbar-nav">
@@ -7,6 +16,10 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../app/listaEstudiantes.php">Lista de estudiantes
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link icon-close" href="../includes/salir.php">Salir
           </a>
         </li>
       </ul>
