63c5de47f609c426fc37494ad7edbc8cb7d20dda
leiber bertel <leiberbertel777@gmail.com>
2023-12-11 20:05:53 -0500

    feat: add configuration for alerts

--- src/app/editar.php
+++ src/app/editar.php
@@ -1,4 +1,6 @@
 <?php
+$editado = false;
+
 require_once "../../config.php";
 
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
@@ -7,7 +9,7 @@ $apellido_paterno = "";
 $apellido_materno = "";
 
 if($id) {
-    $stmt = $con->prepare("SELECT * FROM alumnos WHERE id_alumnos = ?");
+    $stmt = $conn->prepare("SELECT * FROM alumnos WHERE id = ?");
     $stmt->bind_param("i", $id);
     
     if($stmt->execute()) {
@@ -23,17 +25,18 @@ if($id) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nombres = $con->real_escape_string($_POST['nombres']);
-    $apellido_paterno = $con->real_escape_string($_POST['apellido_paterno']);
-    $apellido_materno = $con->real_escape_string($_POST['apellido_materno']);
+    $nombres = $conn->real_escape_string($_POST['nombres']);
+    $apellido_paterno = $conn->real_escape_string($_POST['apellido_paterno']);
+    $apellido_materno = $conn->real_escape_string($_POST['apellido_materno']);
     
-    $stmt = $con->prepare("UPDATE alumnos SET nombres=?, apellido_paterno=?, apellido_materno=? WHERE id_alumnos=?");
+    $stmt = $conn->prepare("UPDATE alumnos SET nombres=?, apellido_paterno=?, apellido_materno=? WHERE id = ?");
     $stmt->bind_param("sssi", $nombres, $apellido_paterno, $apellido_materno, $id);
     
     if ($stmt->execute()) {
+        $editado = true;
         header('location: listaEstudiantes.php');
     } else {
-        echo 'Error actualizando el registro: ' . $stmt->error;
+        $editado = false;
     }
     $stmt->close();
 }
@@ -45,21 +48,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <title>Actualizar estudiante</title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <link rel="shortcut icon" href="school.png" type="image/x-icon">
-  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+  <link rel="shortcut icon" href="../assets/img/school.png" type="image/x-icon">
+  <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body>
-  <nav class="navbar navbar-expand-sm nav-header">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="../index.html">Registro</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="listaEstudiantes.php">Lista de estudiantes</a>
-      </li>
-    </ul>
-  </nav>
-
+  <?php include "../includes/nav.php"; ?>
   <section class="container mt-4 w-50">
     <h3 class="text-center">Actualizar estudiante</h3>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
@@ -79,17 +73,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </form>
   </section>
   <section class="container">
-    <footer class="py-3 my-5">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="../index.html" class="nav-link px-2">Registro</a></li>
-        <li class="nav-item"><a href="listaEstudiantes.php" class="nav-link px-2">Lista de estudiantes</a></li>
-      </ul>
-      <p class="text-center">&copy; 2023 <a class="text-light" target="_blank"
-          href="https://leiberbertel.github.io">Leiber Bertel</a></p>
-    </footer>
+    <?php include "../includes/footer.php"; ?>
   </section>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <?php if ($editado): ?>
+  <script>
+    swal.fire({
+      title: '¡Excelente!',
+      text: 'El Estudiante ha sido actualizado exitosamente',
+      icon: 'success',
+      confirmButtonText: 'Ok',
+      customClass: {
+        popup: 'popup-class',
+        title: 'title-class',
+        confirmButton: 'button-class'
+      }
+    });
+  </script>
+  <?php endif; ?>
+
+  <?php if (!$editado): ?>
+  <script>
+    swal.fire({
+      title: '¡Oopps!',
+      text: 'Algo ha salido mal',
+      icon: 'danger',
+      confirmButtonText: 'Ok',
+      customClass: {
+        popup: 'popup-class',
+        title: 'title-class',
+        confirmButton: 'button-class'
+      }
+    });
+  </script>
+  <?php endif; ?>
+
 </body>
 </html>
