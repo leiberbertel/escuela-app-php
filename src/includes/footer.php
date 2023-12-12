3800cf1dda91d666b028845859420e2ebed9e03e
leiber bertel <leiberbertel777@gmail.com>
2023-12-11 19:58:07 -0500

    style: change the top margin of the footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,4 +1,4 @@
-<footer class="py-3 my-5">
+<footer class="py-3 my-3">
       <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="../app/register.php" class="nav-link px-2">Registro</a></li>
         <li class="nav-item"><a href="../app/listaEstudiantes.php" class="nav-link px-2">Lista de estudiantes</a></li>
